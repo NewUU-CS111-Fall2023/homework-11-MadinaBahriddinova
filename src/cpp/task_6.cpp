b66a8b4812bc00e3a9179fc0e8058ee20c25f10f
MadinaBahriddinova <127374347+MadinaBahriddinova@users.noreply.github.com>
2024-01-23 22:35:03 +0500

    Update task_6.cpp

--- src/cpp/task_6.cpp
+++ src/cpp/task_6.cpp
@@ -1,9 +1,35 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 class Problem6 {
 public:
+    bool isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    vector<int> generatePrimesGreaterThanN(int n) {
+        vector<int> primes;
+
+        int num = n + 1;
+        while (primes.size() < 2) {
+            if (isPrime(num)) {
+                primes.push_back(num);
+            }
+            num++;
+        }
+
+        return primes;
+    }
 };
