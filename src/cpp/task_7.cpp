70dee84943f0e6e2c43dab0c39daa362f7ee6cb7
MadinaBahriddinova <127374347+MadinaBahriddinova@users.noreply.github.com>
2024-01-23 22:36:26 +0500

    Update task_7.cpp

--- src/cpp/task_7.cpp
+++ src/cpp/task_7.cpp
@@ -1,9 +1,11 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+
+using namespace std;
 
 class Problem7 {
 public:
+    int eulerFunction(int a, int b) {
+        return (a - 1) * (b - 1);
+    }
 };
+
