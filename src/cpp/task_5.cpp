802988c3384517ca9a3d866a99af102b7c7981cb
MadinaBahriddinova <127374347+MadinaBahriddinova@users.noreply.github.com>
2024-01-23 22:33:55 +0500

    Update task_5.cpp

--- src/cpp/task_5.cpp
+++ src/cpp/task_5.cpp
@@ -1,8 +1,31 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
 
 class Problem5 {
+public:
+    string decodeMessage(string key, string message) {
+        unordered_map<char, char> substitutionTable;
+        int idx = 0;
+        for (char ch : key) {
+            if (ch != ' ' && substitutionTable.count(ch) == 0) {
+                substitutionTable[ch] = 'a' + idx;
+                idx++;
+            }
+        }
+
+        string decodedMessage;
+        for (char ch : message) {
+            if (ch == ' ') {
+                decodedMessage += ' ';
+            } else {
+                decodedMessage += substitutionTable[ch];
+            }
+        }
+
+        return decodedMessage;
+    }
 };
+
