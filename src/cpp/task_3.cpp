335569788ff2fad9bf661ccbd3accf34b598d3ac
MadinaBahriddinova <127374347+MadinaBahriddinova@users.noreply.github.com>
2024-01-23 22:31:38 +0500

    Update task_3.cpp

--- src/cpp/task_3.cpp
+++ src/cpp/task_3.cpp
@@ -1,8 +1,27 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
 
 class Problem3 {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+
+        int n = s.length();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.count(s.substr(j, i - j))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
 };
