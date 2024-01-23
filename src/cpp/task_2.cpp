26d76d7abf9704ecff39b73607aa72cf9e69e053
MadinaBahriddinova <127374347+MadinaBahriddinova@users.noreply.github.com>
2024-01-23 22:30:12 +0500

    Update task_2.cpp

--- src/cpp/task_2.cpp
+++ src/cpp/task_2.cpp
@@ -1,9 +1,29 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
 
 class Problem2 {
 public:
+    bool canPartition(vector<int>& nums) {
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+
+        if (totalSum % 2 != 0) {
+            return false;
+        }
+
+        int targetSum = totalSum / 2;
+        int n = nums.size();
+        vector<bool> dp(targetSum + 1, false);
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int j = targetSum; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+
+        return dp[targetSum];
+    }
 };
